f0e2b63d41015a2fed67af44051bec1acf2eb776
Alex Chubaty <alex.chubaty@gmail.com>
2013-10-05 22:08:22 -0700

    fixed drift to correctly handle zero population sizes

--- drift/server.R
+++ drift/server.R
@@ -12,12 +12,31 @@ shinyServer(function(input, output) {
       NAa <- round(N*2*p[t]*(1-p[t]))
       Naa <- round(N*(1-p[t])^2)
       
-      NAA.prime <- sum(rbinom(n=NAA, size=1, prob=d))
-      NAa.prime <- sum(rbinom(n=NAa, size=1, prob=d))
-      Naa.prime <- sum(rbinom(n=Naa, size=1, prob=d))
+      if (NAA <= 0) {
+        NAA <- 0
+        NAA.prime <- 0
+      } else {
+        NAA.prime <- sum(rbinom(n=NAA, size=1, prob=d))
+      }
+      if (NAa <= 0) {
+        NAa <- 0
+        NAa.prime <- 0
+      } else {
+        NAa.prime <- sum(rbinom(n=NAa, size=1, prob=d))
+      }
+      if (Naa <= 0) {
+        Naa <- 0
+        Naa.prime <- 0
+      } else {
+        Naa.prime <- sum(rbinom(n=Naa, size=1, prob=d))
+      }
       N.prime <- NAA.prime + NAa.prime + Naa.prime
       
-      p[t+1] <- (2*NAA.prime + NAa.prime) / (2*N.prime)
+      if (N.prime <= 0) {
+        p[t+1] <- NA
+      } else {
+        p[t+1] <- (2*NAA.prime + NAa.prime) / (2*N.prime)
+      }
     }
     
     return(p)
